59674a643424fc580acc5665f9942a305b61bd03
FOKOU DEFO Roberto <robertofokou4@gmail.com>
2025-01-24 13:52:50 +0100

    api pour modifier le risque

--- api/controllers/excel.controller.js
+++ api/controllers/excel.controller.js
@@ -1,183 +1,270 @@
-const fs = require('fs');
-const ExcelService = require('../services/excel.service'); // Assurez-vous que ce chemin est correct
-
+const fs = require("fs");
+const ExcelService = require("../services/excel.service"); // Assurez-vous que ce chemin est correct
+const EntityRiskControl = require("../models/entityRiskControl.model");
 // Contrôleur pour uploader et sauvegarder un fichier Excel
 exports.extractDataFromExcel = (req, res) => {
-    const file = req.file; // Obtenir le fichier téléchargé
+  const file = req.file; // Obtenir le fichier téléchargé
 
-    if (!file) {
-        return res.status(400).json({
-            success: false,
-            message: "Aucun fichier n'a été téléchargé."
-        });
-    }
+  if (!file) {
+    return res.status(400).json({
+      success: false,
+      message: "Aucun fichier n'a été téléchargé.",
+    });
+  }
 
-    const excelService = new ExcelService(file); // Passer le fichier ici
-    const data = excelService.readExcelFile();
+  const excelService = new ExcelService(file); // Passer le fichier ici
+  const data = excelService.readExcelFile();
 
-    if (data) {
-        return res.status(200).json({
-            success: true,
-            message: "Données extraites avec succès.",
-            data
-        });
-    } else {
-        return res.status(500).json({
-            success: false,
-            message: "Erreur lors de l'extraction des données."
-        });
-    }
+  if (data) {
+    return res.status(200).json({
+      success: true,
+      message: "Données extraites avec succès.",
+      data,
+    });
+  } else {
+    return res.status(500).json({
+      success: false,
+      message: "Erreur lors de l'extraction des données.",
+    });
+  }
 };
 
 exports.getEntityRiskControlsByEntityName = async (req, res) => {
-    const { entityName } = req.body; // Le nom de l'entité vient du corps de la requête
+  const { entityName } = req.body; // Le nom de l'entité vient du corps de la requête
 
-    if (!entityName) {
-        return res.status(400).json({
-            success: false,
-            message: "Le nom de l'entité est requis dans le corps de la requête."
-        });
+  if (!entityName) {
+    return res.status(400).json({
+      success: false,
+      message: "Le nom de l'entité est requis dans le corps de la requête.",
+    });
+  }
+
+  const excelService = new ExcelService();
+
+  try {
+    // Appel à la méthode pour récupérer les risques et contrôles de l'entité
+    const entityRiskControls =
+      await excelService.getEntityRiskControlsByEntityName(entityName);
+
+    if (!entityRiskControls || entityRiskControls.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `Aucune donnée trouvée pour l'entité : ${entityName}`,
+      });
     }
 
-    const excelService = new ExcelService();
-
-    try {
-        // Appel à la méthode pour récupérer les risques et contrôles de l'entité
-        const entityRiskControls = await excelService.getEntityRiskControlsByEntityName(entityName);
-
-        if (!entityRiskControls || entityRiskControls.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: `Aucune donnée trouvée pour l'entité : ${entityName}`
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            data: entityRiskControls
-        });
-    } catch (error) {
-        console.error(`Erreur lors de la récupération des données pour l'entité '${entityName}':`, error.message);
-        res.status(500).json({
-            success: false,
-            message: "Erreur lors de la récupération des données",
-            error: error.message
-        });
-    }
+    res.status(200).json({
+      success: true,
+      data: entityRiskControls,
+    });
+  } catch (error) {
+    console.error(
+      `Erreur lors de la récupération des données pour l'entité '${entityName}':`,
+      error.message
+    );
+    res.status(500).json({
+      success: false,
+      message: "Erreur lors de la récupération des données",
+      error: error.message,
+    });
+  }
 };
 
-// Contrôleur pour récupérer les risques et contrôles d’une entité par ID 
+// Contrôleur pour récupérer les risques et contrôles d’une entité par ID
 exports.getEntityRiskControlById = async (req, res) => {
-    const { entityRefId } = req.params;
-    const excelService = new ExcelService();
+  const { entityRefId } = req.params;
+  const excelService = new ExcelService();
 
-    try {
-        // Appelle le service pour récupérer les données
-        const entityRiskControl = await excelService.getEntityRiskControlById(entityRefId);
+  try {
+    // Appelle le service pour récupérer les données
+    const entityRiskControl = await excelService.getEntityRiskControlById(
+      entityRefId
+    );
 
-        if (!entityRiskControl) {
-            return res.status(404).json({ success: false, message: "Aucune donnée trouvée pour cette entité." });
-        }
-
-        res.status(200).json({ success: true, data: entityRiskControl });
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Erreur lors de la récupération des données", error: error.message });
+    if (!entityRiskControl) {
+      return res.status(404).json({
+        success: false,
+        message: "Aucune donnée trouvée pour cette entité.",
+      });
     }
+
+    res.status(200).json({ success: true, data: entityRiskControl });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Erreur lors de la récupération des données",
+      error: error.message,
+    });
+  }
 };
 
 exports.copyRiskOrControls = async (req, res) => {
-    const { itemIds, targetEntityId, itemType } = req.body;
-    const excelService = new ExcelService();
-  
-    try {
-      if (!itemIds || !Array.isArray(itemIds) || !targetEntityId || !itemType) {
-        return res.status(400).json({
-          success: false,
-          message: "Les paramètres itemIds (tableau), targetEntityId et itemType sont requis.",
-        });
-      }
-  
-      // Vérifie que le type est valide
-      if (!['risk', 'control'].includes(itemType)) {
-        return res.status(400).json({
-          success: false,
-          message: "Le type spécifié doit être 'risk' ou 'control'.",
-        });
-      }
-  
-      // Appelle le service pour copier les risques/contrôles
-      const copiedItems = await excelService.copyRiskOrControls(itemIds, targetEntityId, itemType);
-  
-      if (!copiedItems.success) {
-        return res.status(400).json({
-          success: false,
-          message: copiedItems.message,
-          data: copiedItems.data,
-        });
-      }
-  
-      // Retourne la réponse avec succès et les éléments copiés
-      return res.status(200).json({
-        success: true,
-        message: `${itemType === 'risk' ? 'Risques' : 'Contrôles'} copiés avec succès.`,
+  const { itemIds, targetEntityId, itemType } = req.body;
+  const excelService = new ExcelService();
+
+  try {
+    if (!itemIds || !Array.isArray(itemIds) || !targetEntityId || !itemType) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Les paramètres itemIds (tableau), targetEntityId et itemType sont requis.",
+      });
+    }
+
+    // Vérifie que le type est valide
+    if (!["risk", "control"].includes(itemType)) {
+      return res.status(400).json({
+        success: false,
+        message: "Le type spécifié doit être 'risk' ou 'control'.",
+      });
+    }
+
+    // Appelle le service pour copier les risques/contrôles
+    const copiedItems = await excelService.copyRiskOrControls(
+      itemIds,
+      targetEntityId,
+      itemType
+    );
+
+    if (!copiedItems.success) {
+      return res.status(400).json({
+        success: false,
+        message: copiedItems.message,
         data: copiedItems.data,
       });
-    } catch (error) {
-      console.error("Erreur lors de la copie :", error);
-      return res.status(500).json({
+    }
+
+    // Retourne la réponse avec succès et les éléments copiés
+    return res.status(200).json({
+      success: true,
+      message: `${
+        itemType === "risk" ? "Risques" : "Contrôles"
+      } copiés avec succès.`,
+      data: copiedItems.data,
+    });
+  } catch (error) {
+    console.error("Erreur lors de la copie :", error);
+    return res.status(500).json({
+      success: false,
+      message: "Erreur lors de la copie des éléments.",
+      error: error.message,
+    });
+  }
+};
+
+exports.moveRiskOrControls = async (req, res) => {
+  const { itemIds, targetEntityId, itemType } = req.body;
+  const excelService = new ExcelService();
+
+  try {
+    // Validation des paramètres
+    if (!itemIds || !Array.isArray(itemIds) || !targetEntityId || !itemType) {
+      return res.status(400).json({
         success: false,
-        message: "Erreur lors de la copie des éléments.",
-        error: error.message,
+        message:
+          "Les paramètres itemIds (tableau), targetEntityId et itemType sont requis.",
       });
     }
-  };
-  
-  exports.moveRiskOrControls = async (req, res) => {
-    const { itemIds, targetEntityId, itemType } = req.body;
-    const excelService = new ExcelService();
-  
-    try {
-      // Validation des paramètres
-      if (!itemIds || !Array.isArray(itemIds) || !targetEntityId || !itemType) {
-        return res.status(400).json({
-          success: false,
-          message: "Les paramètres itemIds (tableau), targetEntityId et itemType sont requis.",
-        });
-      }
-  
-      // Vérifie que le type est valide
-      if (!['risk', 'control'].includes(itemType)) {
-        return res.status(400).json({
-          success: false,
-          message: "Le type spécifié doit être 'risk' ou 'control'.",
-        });
-      }
-  
-      // Appelle le service pour déplacer les risques/contrôles
-      const movedItems = await excelService.moveRiskOrControls(itemIds, targetEntityId, itemType);
-  
-      // Si l'opération échoue (aucun élément déplacé)
-      if (!movedItems.success) {
-        return res.status(400).json({
-          success: false,
-          message: movedItems.message,
-          data: movedItems.data,
-        });
-      }
-  
-      // Retourne la réponse avec succès et les éléments déplacés
-      return res.status(200).json({
-        success: true,
-        message: `${itemType === 'risk' ? 'Risques' : 'Contrôles'} déplacés avec succès.`,
+
+    // Vérifie que le type est valide
+    if (!["risk", "control"].includes(itemType)) {
+      return res.status(400).json({
+        success: false,
+        message: "Le type spécifié doit être 'risk' ou 'control'.",
+      });
+    }
+
+    // Appelle le service pour déplacer les risques/contrôles
+    const movedItems = await excelService.moveRiskOrControls(
+      itemIds,
+      targetEntityId,
+      itemType
+    );
+
+    // Si l'opération échoue (aucun élément déplacé)
+    if (!movedItems.success) {
+      return res.status(400).json({
+        success: false,
+        message: movedItems.message,
         data: movedItems.data,
       });
-    } catch (error) {
-      console.error("Erreur lors du déplacement :", error);
-      return res.status(500).json({
-        success: false,
-        message: "Erreur lors du déplacement des éléments.",
-        error: error.message,
+    }
+
+    // Retourne la réponse avec succès et les éléments déplacés
+    return res.status(200).json({
+      success: true,
+      message: `${
+        itemType === "risk" ? "Risques" : "Contrôles"
+      } déplacés avec succès.`,
+      data: movedItems.data,
+    });
+  } catch (error) {
+    console.error("Erreur lors du déplacement :", error);
+    return res.status(500).json({
+      success: false,
+      message: "Erreur lors du déplacement des éléments.",
+      error: error.message,
+    });
+  }
+};
+
+exports.updateRiskOrControl = async (req, res) => {
+  const { itemIds, itemType, updates } = req.body;
+
+  try {
+    // Validation des paramètres d'entrée
+    if (!itemIds || !itemType || !updates) {
+      return res.status(400).json({
+        message: "Les paramètres itemIds, itemType et updates sont requis.",
       });
     }
-  };
-  
+
+    if (!["risk", "control"].includes(itemType)) {
+      return res.status(400).json({
+        message: "Le type spécifié doit être 'risk' ou 'control'.",
+      });
+    }
+
+    // Recherche de l'élément correspondant dans la base de données
+    const item = await EntityRiskControl.findOne({
+      [`${itemType}s._id`]: itemIds,
+    });
+
+    if (!item) {
+      return res.status(404).json({
+        message: `Aucun ${itemType} trouvé avec l'ID spécifié.`,
+      });
+    }
+
+    // Extraction de la liste des items et recherche de l'élément à modifier
+    const itemList = item[`${itemType}s`];
+    const elementIndex = itemList.findIndex((el) => el._id == itemIds);
+
+    if (elementIndex === -1) {
+      return res.status(404).json({
+        message: `Aucun élément correspondant trouvé pour l'ID spécifié.`,
+      });
+    }
+
+    // Appliquer les modifications à l'élément
+    const elementToUpdate = itemList[elementIndex];
+    Object.assign(elementToUpdate, updates);
+
+    // Sauvegarder les modifications dans la base de données
+    await item.save();
+
+    // Retourner la réponse avec les données mises à jour
+    return res.status(200).json({
+      message: `${
+        itemType === "risk" ? "Risque" : "Contrôle"
+      } mis à jour avec succès.`,
+      data: elementToUpdate,
+    });
+  } catch (error) {
+    console.error("Erreur :", error);
+    return res.status(500).json({
+      message: "Erreur lors de la mise à jour.",
+      error: error.message,
+    });
+  }
+};
--- api/routes/excel.route.js
+++ api/routes/excel.route.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const excelController = require('../controllers/excel.controller');
+const excelController = require("../controllers/excel.controller");
 
-
-const multer = require('multer');
+const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
-const excelRoutes = app => {
+const excelRoutes = (app) => {
   app.use("/risks-controls", router);
 
   // Routes pour l'upload et la gestion des entités Risk-Control
-  router.post('/upload', upload.single('file'), excelController.extractDataFromExcel);
-  
-  router.post('/get-entity', excelController.getEntityRiskControlsByEntityName);
+  router.post(
+    "/upload",
+    upload.single("file"),
+    excelController.extractDataFromExcel
+  );
+
+  router.post("/get-entity", excelController.getEntityRiskControlsByEntityName);
 
   // Récupérer les données pour une entité spécifique
-  router.get('/entity/show', excelController.getEntityRiskControlById);
-  
+  router.get("/entity/show", excelController.getEntityRiskControlById);
+
   // Copier un risque ou un contrôle vers une autre entité
-  router.post('/copy', excelController.copyRiskOrControls);
-  
+  router.post("/copy", excelController.copyRiskOrControls);
+
   // Déplacer un risque ou un contrôle vers une autre entité
-  router.post('/move', excelController.moveRiskOrControls);
+  router.post("/move", excelController.moveRiskOrControls);
+
+  router.put("/update", excelController.updateRiskOrControl);
 };
 
 module.exports = excelRoutes;
- 
