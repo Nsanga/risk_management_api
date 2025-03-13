9b99fec6b2583bb929e5391af1d46e5b5d811f96
FOKOU DEFO Roberto <robertofokou4@gmail.com>
2025-03-13 09:05:06 +0100

    suite api history

--- api/routes/history.route.js
+++ api/routes/history.route.js
@@ -1,5 +1,8 @@
 var express = require("express");
-const { createHistory } = require("../controllers/history.controller");
+const {
+  createHistory,
+  getAllHistory,
+} = require("../controllers/history.controller");
 var router = express.Router();
 createHistory;
 
@@ -7,6 +10,7 @@ const historyRoutes = (app) => {
   app.use("/history", router);
 
   router.post("/postHistory", createHistory);
+  router.get("/getHistory", getAllHistory);
 };
 
 module.exports = { historyRoutes };
