05e6d66774ff91802f860994e50ef982f83ffd0c
Mercure Mekinda <mercuremekinda@gmail.com>
2025-05-07 16:37:47 +0100

    Merge branch 'roberto_dev' of https://github.com/Nsanga/risk_management_api into dev

--- api/services/historyKRI.service.js
+++ api/services/historyKRI.service.js
@@ -31,16 +31,50 @@ async function getAllHistoriqueKri(req, res) {
   }
 }
 
+// async function getAllHistoriqueByIdKeyIndicator(req, res) {
+//   try {
+//     const historique = await Historique.find({
+//       idKeyIndicator: req.body.idKeyIndicator,
+//     });
+//     // res.status(200).json(historique);
+//     res.status(200).json({
+//       statut: 200,
+//       message: "Histique  liée au KRI",
+//       data: historique,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       error: "Erreur lors de la récupération de l'historique: " + error.message,
+//     });
+//   }
+// }
+
 async function getAllHistoriqueByIdKeyIndicator(req, res) {
   try {
     const historique = await Historique.find({
       idKeyIndicator: req.body.idKeyIndicator,
-    });
-    // res.status(200).json(historique);
+    })
+      .sort({ createdAt: -1 })
+      .limit(5);
+
+    let average = 0;
+    if (historique.length > 0) {
+      const numericValues = historique
+        .map((item) => parseFloat(item.value))
+        .filter((val) => !isNaN(val));
+
+      const total = numericValues.reduce((sum, val) => sum + val, 0);
+      average = numericValues.length > 0 ? total / numericValues.length : 0;
+    }
+
     res.status(200).json({
       statut: 200,
-      message: "Histique  liée au KRI",
-      data: historique,
+      message: "Historique liée au KRI (5 derniers éléments)",
+
+      data: {
+        average: average,
+        histories: historique,
+      },
     });
   } catch (error) {
     res.status(500).json({
